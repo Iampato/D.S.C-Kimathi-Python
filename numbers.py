d89cff7cf1ca68c2c7174ef7715a4a0d3dba747a
Iampato <858wpwaweru@gmail.com>
2018-10-06 12:10:34 +0300

    Added some rules

--- numbers.py
+++ numbers.py
@@ -1,6 +1,19 @@
-a = 5
+"""a = 5
 print (a)
  
  #comment
 a = a*5
 print (a)
+
+#wrong variable assign in python
+#cannot start with numbers and symbols suchas @
+"""
+
+#real world example
+my_income = 20000
+tax_rate = 0.1
+
+tax_charge = my_income * tax_rate
+
+print (tax_charge)
+
