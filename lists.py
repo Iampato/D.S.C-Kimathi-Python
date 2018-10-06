4e2d89c2c8d5f306becf921a7f5d4b1a09bb6d87
Iampato <858wpwaweru@gmail.com>
2018-10-06 13:53:15 +0300

    done with lists

--- lists.py
+++ lists.py
@@ -15,8 +15,14 @@ print(myletters)
 myletters[0] = "Z"
 print ("After")
 print(myletters)
-"""
+ 
 myCars = ["Ford",'Mercedes',"toyota"]
-myCars.append("Honda")
-myCars.extend(["Bugatti",'Marserati','bmw'])
+
+myCars.append("Honda") #Adds an element to a list
+
+myCars.extend(["Bugatti",'Marserati','bmw'])#adds a list to a list
 print (myCars)
+"""
+myletters = ['a','b','c','d','e']
+myletters.reverse()
+print(myletters)
