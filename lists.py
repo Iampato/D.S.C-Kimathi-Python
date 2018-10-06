fb014ffef52698e09498a94b35eb5efe5e4d13eb
Iampato <858wpwaweru@gmail.com>
2018-10-06 13:03:26 +0300

    Added basic method will working with strings

---
+++ lists.py
@@ -0,0 +1,22 @@
+"""
+#basic
+myList = ['Mercury','Mars','earth','jupiter']
+myNumbers = [1,2,3,4,5]
+
+print(myList)
+print (myNumbers)
+
+#Basic methods
+
+myletters = ['a','b','c','d','e']
+print ("Before")
+print(myletters)
+
+myletters[0] = "Z"
+print ("After")
+print(myletters)
+"""
+myCars = ["Ford",'Mercedes',"toyota"]
+myCars.append("Honda")
+myCars.extend(["Bugatti",'Marserati','bmw'])
+print (myCars)
